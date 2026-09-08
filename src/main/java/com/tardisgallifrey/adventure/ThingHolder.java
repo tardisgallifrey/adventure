70bc6cc13702c446fa80cdfd9af4024450f0b909
tardisgallifrey <bigmanrunning@gmail.com>
2026-09-08 13:50:10 -0500

    Commit README, Player additions 09/08/26

--- src/main/java/com/tardisgallifrey/adventure/ThingHolder.java
+++ src/main/java/com/tardisgallifrey/adventure/ThingHolder.java
@@ -1,6 +1,5 @@
 package com.tardisgallifrey.adventure;
 
-import com.tardisgallifrey.adventure.utils.ThingandThingHolder;
 
 public class ThingHolder extends Thing {
 
@@ -18,55 +17,6 @@ public class ThingHolder extends Thing {
 
         public void setThingList(ThingList things){
                 this.things = things;
-        }
-
-        public String describeThings( ){
-                thingStr.append( "" );
-                doDescribeThings(this);
-                return thingStr.toString();
-        }
-
-        private void doDescribeThings( ThingHolder th ){
-                ThingList tlist = th.getThingList();
-
-                for( Thing t : tlist ) { 
-                        thingStr.append( "\n" );
-                        if( t instanceof ContainerThing ){
-                                ContainerThing container = ( ContainerThing ) t;
-                                if( container.isOpen() && container.getThingList().size( ) > 0 ){
-                                        thingStr.append( "The " + container.getName( ) + " contains: \n");
-                                        doDescribeThings(container);
-                                }
-                        }
-                }
-                        
-        }
-
-        public ThingandThingHolder findThing( String objectName ){
-                ThingandThingHolder t_and_th = null;
-                t_and_th = findThinginAnyList( this, objectName );
-                return t_and_th;
-        }
-
-        private ThingandThingHolder findThinginAnyList( ThingHolder th, String objectName ){
-                boolean found = false;
-                ThingandThingHolder t_and_th = null;
-
-                for( Thing t : th.getThingList( ) ){
-                        if( t.getName( ).equals( objectName ) ){
-                                t_and_th = new ThingandThingHolder( t, th );
-                                found = true;
-                        }
-                        if( !found ){
-                                 if( th instanceof ContainerThing ){
-                                         ContainerThing container = ( ContainerThing )th;
-                                         if( container.isOpen( ) ){
-                                                 findThinginAnyList( container, objectName );
-                                         }
-                                 }
-                        }
-                }
-                return t_and_th;
         }
 
 }
--- src/test/java/com/tardisgallifrey/adventure/AdventureTests.java
+++ src/test/java/com/tardisgallifrey/adventure/AdventureTests.java
@@ -2,6 +2,7 @@ package com.tardisgallifrey.adventure;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import com.tardisgallifrey.adventure.utils.*;
 
 public class AdventureTests{
 
@@ -12,5 +13,78 @@ public class AdventureTests{
 
                 assertEquals( "rock", thing.getName( )  );
         }
+
+        @Test
+        void testFindThing( ){
+                ThingList inventory = new ThingList( );
+                inventory.add( new Thing("carrot", "a tasty carrot", true, true) );
+                inventory.add( new Thing( "shirt", "a nice satin shirt", true, true) );
+                inventory.add( new Treasure( "ruby", "a large red ruby", 400) );
+                
+                Player newPlayer = new Player("Bob", "a pleasant sort", null, inventory);
+                Thing found = newPlayer.findThing( "carrot", newPlayer.getBag( ) );  
+                assertEquals( "carrot", found.getName( )); 
+                assertNotEquals( "rock", found.getName( )); 
+
+        }
+
+        @Test
+        void testIsThingInInventory( ){
+                ThingList inventory = new ThingList( );
+                inventory.add( new Thing("carrot", "a tasty carrot", true, true) );
+                inventory.add( new Thing( "shirt", "a nice satin shirt", true, true) );
+                inventory.add( new Treasure( "ruby", "a large red ruby", 400) );
+                
+                Player newPlayer = new Player("Bob", "a pleasant sort", null, inventory);
+
+                assertTrue( newPlayer.isThinginInventory("carrot") );
+                assertFalse( newPlayer.isThinginInventory("rock") );
+        }
+
+        @Test
+        void testIsThingInRoom( ){
+                ThingList inventory = new ThingList( );
+                inventory.add( new Thing("carrot", "a tasty carrot", true, true) );
+                inventory.add( new Thing( "shirt", "a nice satin shirt", true, true) );
+                inventory.add( new Treasure( "ruby", "a large red ruby", 400) );
+                
+                Room aRoom = new Room("Hall", "a long hall", Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT, inventory);
+                Player newPlayer = new Player("Bob", "a pleasant sort", aRoom, null);
+
+                assertTrue( newPlayer.isThinginRoom("carrot") );
+                assertFalse( newPlayer.isThinginRoom( "rock" ) );
+        }
+
+        @Test
+        void testIsThingHere( ){
+                ThingList inventory = new ThingList( );
+                inventory.add( new Thing("carrot", "a tasty carrot", true, true) );
+                inventory.add( new Thing( "shirt", "a nice satin shirt", true, true) );
+                inventory.add( new Treasure( "ruby", "a large red ruby", 400) );
+
+                ThingList room = new ThingList( );
+                room.add( new Thing( "sword", "a sharp sword", true, true ) );
+                room.add( new Treasure( "diamond", "shiny", 1000 ) );
+
+                
+                Room aRoom = new Room("Hall", "a long hall", 
+                                Direction.NOEXIT, 
+                                Direction.NOEXIT, 
+                                Direction.NOEXIT, 
+                                Direction.NOEXIT,
+                                room );
+
+                Player newPlayer = new Player("Bob", "a pleasant sort", aRoom, inventory);
+
+                assertTrue( newPlayer.isThingHere( "carrot" ) );
+                assertFalse( newPlayer.isThingHere( "rock" ) );
+                assertTrue( newPlayer.isThingHere( "diamond" ) );
+                assertFalse( newPlayer.isThingHere( "wombat" ) ); 
+
+        } 
+ 
+ 
+
+                
 
 }
